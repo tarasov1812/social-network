1b1ce8eaf2d490893b3ce6591cf34cbfcf02fe15
tarasov1812 <75462526+tarasov1812@users.noreply.github.com>
2023-03-01 20:59:46 +0100

    create function post_size

---
+++ public/assets/post_size.js
@@ -0,0 +1,33 @@
+/**
+ * This function counts all characters in the message, except for links
+ * @param {*} message - user message
+ * @returns number of characters in the message, except for links
+ */
+
+export default function posSize (message) {
+    //dictionary of top-10 most populars domens
+  const domens = [".com", ".org",".net", ".gov", ".edu",  ".es", ".ru", ".de", ".uk", ".nl"];
+    //create array of words if the message for checking if the word has a link
+  const arrayMessage = message.split(" ");
+   //check every word of the message
+  for(let i = 0; i<arrayMessage.length; i++){
+    //if a word has a domain attribute, this word will be removed from the array
+    for(let j = 0; j<domens.length;j++){
+      if(arrayMessage[i]===undefined){
+        continue;
+      } else if(arrayMessage[i].endsWith(domens[j])){
+        arrayMessage.splice(i);
+      } else if(arrayMessage[i].startsWith("https://")){
+        arrayMessage.splice(i);
+      } else if(arrayMessage[i].startsWith("http://")){
+        arrayMessage.splice(i);
+      } else if(arrayMessage[i].startsWith("www.")){
+        arrayMessage.splice(i);
+      }
+    }
+  }
+  //create a string from the modified array
+  const newMessage = arrayMessage.join(" ");
+  //return length of the string
+  return newMessage.length;
+}
