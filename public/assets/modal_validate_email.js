47a7c9b4f4d70dda1d8912f8ef1be4a53ef00428
tarasov1812 <tarasov1812@gmail.com>
2023-04-15 21:36:17 +0200

    test on the server 3

--- public/assets/modal_validate_email.js
+++ public/assets/modal_validate_email.js
@@ -44,8 +44,10 @@ function modalValidateEmail() {
   if (!validateEmail(email)) {
     document.getElementById('email').style.backgroundColor = '#FFDEEC';
     document.getElementById('email').style.border = '1px solid #FF97C3';
+    document.getElementById('invalid-email').style.visibility = 'visible';
   } else {
     document.getElementById('email').style.backgroundColor = '#FFFFFF';
     document.getElementById('email').style.border = '1px solid rgba(223, 223, 223, 1)';
+    document.getElementById('invalid-email').style.visibility = 'hidden';
   }
 }
