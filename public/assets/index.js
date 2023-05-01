b91e7ffe5fec3b4f0c44331da51db333ed711cc3
tarasov1812 <tarasov1812@gmail.com>
2023-05-01 14:00:49 +0200

    create animation of loading messages

--- public/assets/index.js
+++ public/assets/index.js
@@ -65,6 +65,8 @@ function loadStatistic() {
 loadStatistic();
 
 // JSON fetch messages
+const grayMessages = document.getElementById('gray-messages');
+const amoutOfMessagesToShow = 5;
 function loadMessages() {
   // Load date from data.json
   fetch('./data.json')
@@ -74,8 +76,9 @@ function loadMessages() {
       fetch('./pictures.json')
         .then((response) => response.json())
         .then((pictures) => {
-          data.lastMessages.forEach((message) => {
-            const picture = pictures.picturesMessage.find((par) => par.messageId === message.id);
+          for (let i = 0; i < amoutOfMessagesToShow; i += 1) {
+            const picture = pictures.picturesMessage.find((par) => par.messageId
+            === data.lastMessages[i].id);
             const pictureUrl = picture ? picture.urlAvatar : '';
             const postPictureUrl = picture ? picture.urlMessagePic : '';
             const messageHtml = `
@@ -84,42 +87,108 @@ function loadMessages() {
                             <div class="post">
                                 <div class="nick-name-date">
                                     <div class="name-nick">
-                                        <span class="name">${message.author}</span>
-                                        <span class="nick">${message.nickName}</span>
+                                        <span class="name">${data.lastMessages[i].author}</span>
+                                        <span class="nick">${data.lastMessages[i].nickName}</span>
                                     </div>
                                     <div class="date">
-                                        <span class="time">${message.time}</span>
+                                        <span class="time">${data.lastMessages[i].time}</span>
                                         <span class="ago">&nbsp ago</span>
                                     </div>
                                 </div>
                                 <div class="message">
-                                    <p>${message.content}</p>
+                                    <p>${data.lastMessages[i].content}</p>
                                     <img class="attached-picture" src="${postPictureUrl}"/>
                                 </div>
                                 <div class="counters">
                                     <div class="repost">
                                         <img src="img/repost.svg">
-                                        <span>${message.reposts}</span>
+                                        <span>${data.lastMessages[i].reposts}</span>
                                     </div>
                                     <div class="like">
                                         <img src="img/like.svg">
-                                        <span>${message.likes}</span>
+                                        <span>${data.lastMessages[i].likes}</span>
                                     </div>
                                     <div class="share">
                                         <img src="img/share.svg">
-                                        <span>${message.shares}</span>
+                                        <span>${data.lastMessages[i].shares}</span>
                                     </div>
                                 </div>
                             </div>
                         </div>
                         <hr>
             `;
+            grayMessages.classList.add('hidden');
             document.getElementById('all-posts-id').insertAdjacentHTML('beforeend', messageHtml);
-          });
+          }
         })
         .catch((error) => console.error('Error with picture.json', error));
     })
     .catch((error) => console.error('Error with data.json', error));
 }
 
-loadMessages();
+setTimeout(() => {
+  loadMessages();
+}, 3000);
+
+// JSON fetch populas tags
+const grayTopics = document.getElementById('gray-topics');
+const tagsToSHow = 5;
+function loadTags() {
+  fetch('./data.json')
+    .then((response) => response.json())
+    .then((data) => {
+      const topic = data.topics;
+      for (let i = 0; i < tagsToSHow; i += 1) {
+        const currentTopicsHtml = `
+                        <h5>${topic[i].tag}</h5>
+                        <p>${topic[i].messages}</p>
+            `;
+        grayTopics.classList.add('hidden');
+        document.getElementById('current-topics').insertAdjacentHTML('beforeend', currentTopicsHtml);
+      }
+    })
+    .catch((error) => console.error(error));
+}
+
+setTimeout(() => {
+  loadTags();
+}, 3000);
+
+// JSON fetch channels
+const grayChannels = document.getElementById('gray-channels');
+const channelsToShow = 3;
+function loadChannels() {
+  fetch('./data.json')
+    .then((response) => response.json())
+    .then((data) => {
+      const blog = data.blogs;
+      fetch('./pictures.json')
+        .then((response) => response.json())
+        .then((pictures) => {
+          for (let i = 0; i < channelsToShow; i += 1) {
+            const picture = pictures.picturesChannel.find((par) => par.channelId
+          === blog[i].id);
+            const pictureUrl = picture ? picture.url : '';
+            const interestingChannelsHtml = `
+                        <div id ="interesting-channels">
+                            <div class="channel">
+                                <img class="channel-logo" src="${pictureUrl}"/>
+                                <div class="channel-name-and-nick">
+                                    <span class="channel-name">${blog[i].channelName}</span>
+                                    <span class="channel-nick">>${blog[i].channelNick}</span>
+                                </div>
+                                <button class="read">Read</button>
+                            </div>
+            `;
+            grayChannels.classList.add('hidden');
+            document.getElementById('interesting-channels').insertAdjacentHTML('beforeend', interestingChannelsHtml);
+          }
+        })
+        .catch((error) => console.error(error));
+    })
+    .catch((error) => console.error(error));
+}
+
+setTimeout(() => {
+  loadChannels();
+}, 3000);
