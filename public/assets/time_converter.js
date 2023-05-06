619d6557b5efec34379c3df7a7f22e779ea2ea00
tarasov1812 <tarasov1812@gmail.com>
2023-05-06 17:33:56 +0200

    fix style, change messages time to DATE format

--- public/assets/time_converter.js
+++ public/assets/time_converter.js
@@ -11,38 +11,38 @@ export default function timeConverter(number) {
   }
   // one minute singular
   if (number === 1) {
-    return `${number} minute`;
+    return `${number} minute ago`;
   }
 
   // from 2 minutes until 59 minutes  in plural
   if (number > 1 && number < 60) {
-    return `${number} minutes`;
+    return `${number} minutes ago`;
   }
   // one hour singular
   if (number >= 60 && number < 120) {
-    return '1 hour';
+    return '1 hour ago';
   }
 
   // from 2 hours until 23 hours in plural
   if (number >= 120 && number < 1440) {
     const hourNum = Math.floor(number / 60);
-    return `${hourNum} hours`;
+    return `${hourNum} hours ago`;
   }
 
   // one day singular
   if (number >= 1440 && number < 2880) {
-    return '1 day';
+    return '1 day ago';
   }
 
   // from 2 days until 365 days in plural
   if (number >= 2880 && number < 527040) {
     const daysNum = Math.floor(number / 1440);
-    return `${daysNum} days`;
+    return `${daysNum} days ago`;
   }
 
   // any higher numbers is over one year ago
   if (number >= 527040) {
-    return 'over one year';
+    return 'over one year ago';
   }
 
   // in any other case return empty string
